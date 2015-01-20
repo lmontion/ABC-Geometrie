cf662869127d3b83e46042546fd84e8df7008375
Yanick <yanick.servant@free.fr>
2015-01-20 11:11:23 +0100

    Page de jeu

---
+++ android/ABCGeometrie/app/src/main/java/com/abcgeometrie/activity/QuestionActivity.java
@@ -0,0 +1,17 @@
+package com.abcgeometrie.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.abcgeometrie.R;
+
+/**
+ * Created by Yanick on 20/01/2015.
+ */
+public class QuestionActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
