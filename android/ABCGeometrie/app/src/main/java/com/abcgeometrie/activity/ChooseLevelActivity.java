a493590cc7c2a72360567ddd4e5bfcb6b06bc0db
Yanick <yanick.servant@free.fr>
2015-01-20 09:14:45 +0100

    Interface choix du niveau

---
+++ android/ABCGeometrie/app/src/main/java/com/abcgeometrie/activity/ChooseLevelActivity.java
@@ -0,0 +1,17 @@
+package com.abcgeometrie.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.abcgeometrie.R;
+
+/**
+ * Created by Yanick on 19/01/2015.
+ */
+public class ChooseLevelActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_level);
+    }
+}
