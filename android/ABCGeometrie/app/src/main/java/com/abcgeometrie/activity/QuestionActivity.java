eb5d241cee003dd75720155aba9b37435dd12656
Yanick <yanick.servant@free.fr>
2015-01-21 10:02:31 +0100

    Applicationd des polices, mise en place des strings, homogénéisation (P2)

--- android/ABCGeometrie/app/src/main/java/com/abcgeometrie/activity/QuestionActivity.java
+++ android/ABCGeometrie/app/src/main/java/com/abcgeometrie/activity/QuestionActivity.java
@@ -1,8 +1,10 @@
 package com.abcgeometrie.activity;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.abcgeometrie.R;
 
@@ -10,10 +12,19 @@ import com.abcgeometrie.R;
  * Created by Yanick on 20/01/2015.
  */
 public class QuestionActivity extends Activity {
+
+    private TextView txtViewQuestion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Typeface tfLight = Typeface.createFromAsset(getAssets(),"fonts/orbitron-light.otf");
+        Typeface tfMedium = Typeface.createFromAsset(getAssets(),"fonts/orbitron-medium.otf");
+        txtViewQuestion = (TextView) findViewById(R.id.txtViewQuestion);
+
+        txtViewQuestion.setTypeface(tfLight);
     }
 }
